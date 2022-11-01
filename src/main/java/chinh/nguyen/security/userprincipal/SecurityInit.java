4085648e732bdd6f071fc2741ca3118fa498c8c7
Your Name <dat054760@gmail.com>
2022-11-01 10:30:51 +0700

    up

---
+++ src/main/java/chinh/nguyen/security/userprincipal/SecurityInit.java
@@ -0,0 +1,6 @@
+package chinh.nguyen.security.userprincipal;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/chinh/nguyen/security/userprincipal/UserDetailsServiceIMPL.java
@@ -0,0 +1,26 @@
+package chinh.nguyen.security.userprincipal;
+
+import chinh.nguyen.model.User;
+import chinh.nguyen.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+@Service
+public class UserDetailsServiceIMPL implements UserDetailsService {
+    @Autowired
+    IUserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("User Not Found with -> username or email : " + username));
+
+        return UserPrinciple.build(user);
+    }
+}
